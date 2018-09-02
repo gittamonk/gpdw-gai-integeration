e37c106a7c47c0745561b4ca981e74ae22a65e67
Jain <sunny.jain@capgemini.com>
2018-09-02 18:31:51 +0530

    second commit

---
+++ src/main/scala/com/gittamonk/gpdw/hdfs/ApplicationFileSystem.scala
@@ -0,0 +1,8 @@
+package com.gittamonk.gpdw.hdfs
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem,Path,FileStatus,FileUtil}
+
+object ApplicationFileSystem {
+
+}
---
+++ src/main/scala/com/gittamonk/gpdw/utils/data/DataManager.scala
@@ -0,0 +1,9 @@
+package com.gittamonk.gpdw.utils.data
+
+import com.gittamonk.gpdw.utils.hdfs.ApplicationFileSystem
+import com.gittamonk.gpdw.utils.log.Logging
+
+trait DataManager {
+
+
+}
